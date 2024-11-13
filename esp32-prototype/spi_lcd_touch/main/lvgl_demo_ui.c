16fa768e4d266b5c68fc4c504eb32c4d60f8228d
sanchezocegueda <alsanchez6601@gmail.com>
2024-11-12 19:29:37 -0800

    more functionality for the screen

--- esp32-prototype/spi_lcd_touch/main/lvgl_demo_ui.c
+++ esp32-prototype/spi_lcd_touch/main/lvgl_demo_ui.c
@@ -7,6 +7,7 @@
 // This demo UI is adapted from LVGL official example: https://docs.lvgl.io/master/examples.html#loader-with-arc
 
 #include "lvgl.h"
+#include <math.h>
 
 static lv_obj_t * btn;
 static lv_display_rotation_t rotation = LV_DISP_ROTATION_0;
@@ -26,70 +27,77 @@ static void set_angle(void * obj, int32_t v)
 }
 
 
+float get_angle_from_imu() {
+    return -M_PI;
+}
+
 
 void example_lvgl_demo_ui(lv_display_t *disp)
 {
     lv_obj_t *scr = lv_display_get_screen_active(disp);
 
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x020C0E), LV_PART_MAIN);
+
     /* Alex: Try to draw a sample minimap */
-    
-    static lv_style_t style;
-    lv_style_init(&style);
-    lv_style_set_radius(&style, 50);
-    lv_style_set_bg_opa(&style, LV_OPA_COVER);
-    lv_style_set_bg_color(&style, lv_color_hex(0x0000FF));
-    // lv_style_set_shadow_color(&style, lv_color_black());
-    // lv_style_set_shadow_width(&style, 5);
-    // lv_style_set_shadow_offset_x(&style, 20);
-    // lv_style_set_shadow_offset_y(&style, 20);
-    // lv_style_set_shadow_opa(&style, LV_OPA_50);
 
-    lv_obj_t* me = lv_obj_create(lv_screen_active());
-    lv_obj_align(me, LV_ALIGN_CENTER, -20, 0);
 
-    lv_obj_add_style(me, &style, 0);
-    lv_obj_set_size(me, 50, 50);
-    lv_obj_center(me);
+    // Styles
+    static lv_style_t style_bubble;
+    lv_style_init(&style_bubble);
+    lv_style_set_radius(&style_bubble, 20);
+    lv_style_set_size(&style_bubble, 20, 20);
+    // lv_style_set_transform_rotation();
+    lv_style_set_bg_opa(&style_bubble, LV_OPA_COVER);
+    lv_style_set_bg_color(&style_bubble, lv_color_hex(0x23CEF1));
 
-    lv_obj_t* me_label = lv_label_create(me);
+    static lv_style_t style_name;
 
+
+    // Me
+
+    // lv_obj_t* me_container = lv_obj_create(lv_screen_active());
+
+
+    lv_obj_t* me_bubble = lv_obj_create(lv_screen_active());
+    // lv_obj_center(me_bubble);
+    int screen_radius = 100;// lv_display_get_horizontal_resolution((lv_display_t*) lv_screen_active());
+    float north_angle = get_angle_from_imu();
+    int32_t north_x = screen_radius * cos(north_angle);
+    int32_t north_y = -(screen_radius * sin(north_angle));
+    lv_obj_align(me_bubble, LV_ALIGN_CENTER, north_x, north_y);
+    lv_obj_add_style(me_bubble, &style_bubble, 0);
+    lv_obj_set_style_radius(me_bubble, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_size(me_bubble, 20, 20);
+
+    lv_obj_t* me_label = lv_label_create(lv_screen_active());
     lv_label_set_text(me_label, "me");
+    lv_obj_set_size(me_label, 30, 30);
+    lv_obj_set_style_text_color(me_label, lv_color_hex(0xffffff), LV_PART_MAIN);
+    lv_obj_align_to(me_label, me_bubble, LV_ALIGN_OUT_TOP_LEFT, 20, 20);
 
-    lv_obj_t* friend = lv_obj_create(lv_screen_active());
-    lv_obj_t* friend_label = lv_label_create(friend);
-
-    lv_obj_add_style(friend, &style, 0);
-    lv_obj_set_size(friend, 50, 50);
-
-
-    lv_obj_align(friend, LV_ALIGN_RIGHT_MID, -20, 0);
-    lv_label_set_text(friend_label, "fren");
-    /* end of my shenanigans */
+    // lv_obj_set_style_opa(me_container, 0, 0);
 
 
-    // btn = lv_button_create(scr);
-    // lv_obj_t * lbl = lv_label_create(btn);
-    // lv_label_set_text_static(lbl, LV_SYMBOL_REFRESH" ROTATE");
-    // lv_obj_align(btn, LV_ALIGN_BOTTOM_LEFT, 30, -30);
-    // /*Button event*/
-    // lv_obj_add_event_cb(btn, btn_cb, LV_EVENT_CLICKED, disp);
+    // Friend
+
+    static lv_style_t style_friend;
+    lv_style_init(&style_friend);
+    lv_style_set_radius(&style_friend, 25);
+    lv_style_set_bg_opa(&style_friend, LV_OPA_COVER);
+    lv_style_set_bg_color(&style_friend, lv_color_hex(0xE92626));
 
 
-    // /*Create an Arc*/
-    // lv_obj_t * arc = lv_arc_create(scr);
-    // lv_arc_set_rotation(arc, 270);
-    // lv_arc_set_bg_angles(arc, 0, 360);
-    // // lv_obj_remove_style(arc, NULL, LV_PART_KNOB);   /*Be sure the knob is not displayed*/
-    // lv_obj_remove_flag(arc, LV_OBJ_FLAG_CLICKABLE);  /*To not allow adjusting by click*/
-    // lv_obj_center(arc);
+    // lv_obj_align(me_label, LV_ALIGN_CENTER, -20, 0);
 
-    // lv_anim_t a;
-    // lv_anim_init(&a);
-    // lv_anim_set_var(&a, arc);
-    // lv_anim_set_exec_cb(&a, set_angle);
-    // lv_anim_set_duration(&a, 1000);
-    // lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);    /*Just for the demo*/
-    // lv_anim_set_repeat_delay(&a, 500);
-    // lv_anim_set_values(&a, 0, 100);
-    // lv_anim_start(&a);
+
+    // Add north indicator
+    // Assume you already have an angle from the IMU
+
+
+
+    // lv_obj_align(?, LV_ALIGN_CENTER, 20, 30);
+
+
+
+    
 }
