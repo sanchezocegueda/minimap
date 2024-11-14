7438e47f8d708ff9015404fc0b47676e823c651e
sanchezocegueda <alsanchez6601@gmail.com>
2024-11-14 09:00:09 -0800

    four fake ppl on minimap screen

--- esp32-prototype/spi_lcd_touch/main/lvgl_demo_ui.c
+++ esp32-prototype/spi_lcd_touch/main/lvgl_demo_ui.c
@@ -28,7 +28,7 @@ static void set_angle(void * obj, int32_t v)
 
 
 float get_angle_from_imu() {
-    return -M_PI;
+    return M_PI/4;
 }
 
 
@@ -50,6 +50,30 @@ void example_lvgl_demo_ui(lv_display_t *disp)
     lv_style_set_bg_opa(&style_bubble, LV_OPA_COVER);
     lv_style_set_bg_color(&style_bubble, lv_color_hex(0x23CEF1));
 
+    static lv_style_t style_person1;
+    lv_style_init(&style_person1);
+    lv_style_set_radius(&style_person1, 20);
+    lv_style_set_size(&style_person1, 20, 20);
+    // lv_style_set_transform_rotation();
+    lv_style_set_bg_opa(&style_person1, LV_OPA_COVER);
+    lv_style_set_bg_color(&style_person1, lv_color_hex(0x21C143));
+
+    static lv_style_t style_person2;
+    lv_style_init(&style_person2);
+    lv_style_set_radius(&style_person2, 20);
+    lv_style_set_size(&style_person2, 20, 20);
+    // lv_style_set_transform_rotation();
+    lv_style_set_bg_opa(&style_person2, LV_OPA_COVER);
+    lv_style_set_bg_color(&style_person2, lv_color_hex(0xEA3535));
+
+    static lv_style_t style_person3;
+    lv_style_init(&style_person3);
+    lv_style_set_radius(&style_person3, 20);
+    lv_style_set_size(&style_person3, 20, 20);
+    // lv_style_set_transform_rotation();
+    lv_style_set_bg_opa(&style_person3, LV_OPA_COVER);
+    lv_style_set_bg_color(&style_person3, lv_color_hex(0xDAEA1D));
+
     static lv_style_t style_name;
 
 
@@ -59,32 +83,85 @@ void example_lvgl_demo_ui(lv_display_t *disp)
 
 
     lv_obj_t* me_bubble = lv_obj_create(lv_screen_active());
-    // lv_obj_center(me_bubble);
-    int screen_radius = 100;// lv_display_get_horizontal_resolution((lv_display_t*) lv_screen_active());
-    float north_angle = get_angle_from_imu();
-    int32_t north_x = screen_radius * cos(north_angle);
-    int32_t north_y = -(screen_radius * sin(north_angle));
-    lv_obj_align(me_bubble, LV_ALIGN_CENTER, north_x, north_y);
+    lv_obj_center(me_bubble);
+    // int screen_radius = 100;// lv_display_get_horizontal_resolution((lv_display_t*) lv_screen_active());
+    // float north_angle = get_angle_from_imu();
+    // int32_t north_x = screen_radius * cos(north_angle);
+    // int32_t north_y = -(screen_radius * sin(north_angle));
+    // lv_obj_align(me_bubble, LV_ALIGN_CENTER, north_x, north_y);
     lv_obj_add_style(me_bubble, &style_bubble, 0);
     lv_obj_set_style_radius(me_bubble, LV_RADIUS_CIRCLE, 0);
     lv_obj_set_size(me_bubble, 20, 20);
 
     lv_obj_t* me_label = lv_label_create(lv_screen_active());
-    lv_label_set_text(me_label, "me");
+    lv_label_set_text(me_label, "You");
     lv_obj_set_size(me_label, 30, 30);
     lv_obj_set_style_text_color(me_label, lv_color_hex(0xffffff), LV_PART_MAIN);
-    lv_obj_align_to(me_label, me_bubble, LV_ALIGN_OUT_TOP_LEFT, 20, 20);
+    lv_obj_align_to(me_label, me_bubble, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
+
+    // lv_obj_center(me_bubble);
+    // int screen_radius = 100;// lv_display_get_horizontal_resolution((lv_display_t*) lv_screen_active());
+    int screen_radius = 70;// lv_display_get_horizontal_resolution((lv_display_t*) lv_screen_active());
+    
+    /* Person 1 */
+
+    // float north_angle = get_angle_from_imu();
+    lv_obj_t* person1_bubble = lv_obj_create(lv_screen_active());
+    float person1_angle = M_PI;
+    int32_t person1_x = screen_radius * cos(person1_angle);
+    int32_t person1_y = -(screen_radius * sin(person1_angle));
+    lv_obj_align(person1_bubble, LV_ALIGN_CENTER, person1_x, person1_y);
+    lv_obj_add_style(person1_bubble, &style_person1, 0);
+    lv_obj_set_style_radius(person1_bubble, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_size(person1_bubble, 20, 20);
+    // lv_style_set_bg_color(person1_bubble, lv_color_hex(0x21C143));
+
+    lv_obj_t* person1_label = lv_label_create(lv_screen_active());
+    lv_label_set_text(person1_label, "Person 1");
+    lv_obj_set_size(person1_label, 60, 30);
+    lv_obj_set_style_text_color(person1_label, lv_color_hex(0xffffff), LV_PART_MAIN);
+    lv_obj_align_to(person1_label, person1_bubble, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
 
     // lv_obj_set_style_opa(me_container, 0, 0);
 
+    /* Person 2 */
 
-    // Friend
+    lv_obj_t* person2_bubble = lv_obj_create(lv_screen_active());
+    float person2_angle = M_PI_4;
+    int32_t person2_x = screen_radius * cos(person2_angle);
+    int32_t person2_y = -(screen_radius * sin(person2_angle));
+    lv_obj_align(person2_bubble, LV_ALIGN_CENTER, person2_x, person2_y);
+    lv_obj_add_style(person2_bubble, &style_person2, 0);
+    lv_obj_set_style_radius(person2_bubble, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_size(person2_bubble, 20, 20);
+    // lv_style_set_bg_color(person2_bubble, lv_color_hex(0xEA3535));
 
-    static lv_style_t style_friend;
-    lv_style_init(&style_friend);
-    lv_style_set_radius(&style_friend, 25);
-    lv_style_set_bg_opa(&style_friend, LV_OPA_COVER);
-    lv_style_set_bg_color(&style_friend, lv_color_hex(0xE92626));
+    lv_obj_t* person2_label = lv_label_create(lv_screen_active());
+    // lv_obj_set_style_text_font(person2_label, &lv_font_montserrat_1, 0);
+    lv_label_set_text(person2_label, "Person 2");
+    lv_obj_set_size(person2_label, 70, 30);
+    lv_obj_set_style_text_color(person2_label, lv_color_hex(0xffffff), LV_PART_MAIN);
+    lv_obj_align_to(person2_label, person2_bubble, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
+
+    /* Person 3 */
+
+
+    lv_obj_t* person3_bubble = lv_obj_create(lv_screen_active());
+    float person3_angle = 5 * M_PI_4;
+    int32_t person3_x = screen_radius * cos(person3_angle);
+    int32_t person3_y = -(screen_radius * sin(person3_angle));
+    lv_obj_align(person3_bubble, LV_ALIGN_CENTER, person3_x, person3_y);
+    lv_obj_add_style(person3_bubble, &style_person3, 0);
+    lv_obj_set_style_radius(person3_bubble, LV_RADIUS_CIRCLE, 0);
+    lv_obj_set_size(person3_bubble, 20, 20);
+    // lv_style_set_bg_color(person3_bubble, lv_color_hex(0xDAEA1D));
+
+    lv_obj_t* person3_label = lv_label_create(lv_screen_active());
+    // lv_obj_set_style_text_font(person2_label, &lv_font_montserrat_1, 0);
+    lv_label_set_text(person3_label, "Person 3");
+    lv_obj_set_size(person3_label, 70, 30);
+    lv_obj_set_style_text_color(person3_label, lv_color_hex(0xffffff), LV_PART_MAIN);
+    lv_obj_align_to(person3_label, person3_bubble, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
 
 
     // lv_obj_align(me_label, LV_ALIGN_CENTER, -20, 0);
