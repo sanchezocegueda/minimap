6ef8ec7055dc020fdc5d7c1be3dd20b5526f8fe4
Luca Poulos <lucapoulos@berkeley.edu>
2024-11-19 19:23:34 -0800

    lora counter around cory

--- esp32-prototype/lora-library/main/lora-library.c
+++ esp32-prototype/lora-library/main/lora-library.c
@@ -2,13 +2,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "lora.h"
+#include "esp_log.h"
+
+static uint32_t counter = 0;
 
 void task_tx(void *p)
 {
    for(;;) {
-      vTaskDelay(pdMS_TO_TICKS(5000));
-      lora_send_packet((uint8_t*)"Hello", 5);
-      printf("packet sent...\n");
+      lora_send_packet((uint8_t*)&counter, 7);
+      ESP_LOGI("[SENT]", "counter: %ld", counter);
+      vTaskDelay(pdMS_TO_TICKS(2500));
+      counter++;
    }
 }
 
@@ -21,8 +25,10 @@ void task_rx(void *p)
       lora_receive();    // put into receive mode
       while(lora_received()) {
          x = lora_receive_packet(buf, sizeof(buf));
-         buf[x] = 0;
-         printf("Received: %s\n", buf);
+         // buf[x] = 0;
+         // printf("Received: %s\n", buf);
+
+         ESP_LOGI("[RECEIVED]", "%d bytes, counter: %ld", x, (uint32_t) buf[0]);
          lora_receive();
       }
       vTaskDelay(1);
@@ -34,6 +40,6 @@ void app_main()
    lora_init();
    lora_set_frequency(915e6);
    lora_enable_crc();
-//    xTaskCreate(&task_tx, "task_tx", 2048, NULL, 5, NULL);
+   // xTaskCreate(&task_tx, "task_tx", 2048, NULL, 5, NULL);
    xTaskCreate(&task_rx, "task_rx", 2048, NULL, 5, NULL);
 }
